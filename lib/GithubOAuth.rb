0bd227bfcfddeb7c72089844e3bd8ebb09b95275
joseph <josephtkao@gmail.com>
2021-03-23 17:33:06 -0500

    Initial commit

---
+++ lib/GithubOAuth.rb
@@ -0,0 +1,6 @@
+require "GithubOAuth/version"
+
+module GithubOAuth
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/GithubOAuth/version.rb
@@ -0,0 +1,3 @@
+module GithubOAuth
+  VERSION = "0.1.0"
+end
