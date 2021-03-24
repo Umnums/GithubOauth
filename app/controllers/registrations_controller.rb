07413c2514687584ea30e34c592488880795f700
joseph <josephtkao@gmail.com>
2021-03-24 10:44:59 -0500

    added views via devise

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,26 @@
 class RegistrationsController < ApplicationController
+    def new
+
+    end
+    
+    def create
+
+    end
+
+    def update
+
+    end
+
+    def edit
+        
+    end
+    private
+    
+    def signup_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def account_update_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
 end
